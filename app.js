7021619d091e80ad7121a1ec55328873593e4826
Nick Fogle <nickfogle@gmail.com>
2017-09-29 23:29:13 -0400

    Migrate from OpenShift

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-var express = require('express')
-, app = express()
-, server = require('http').createServer(app)
-, routes = require('./routes')
-, chatServer =require('./chatServer')(server);
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 
-app.configure(function() {
-  app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8000);
-  app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
-  app.use('/components', express.static(__dirname + '/components'));
-  app.use('/js', express.static(__dirname + '/js'));
-  app.use('/icons', express.static(__dirname + '/icons'));
-  app.use(app.router);
-  app.set('view engine', 'ejs');
-});
+var app = express();
+var server = require('http').createServer(app);
+var routes = require('./routes');
+var chatServer =require('./chatServer')(server);
+
+app.set('view engine', 'ejs');
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(__dirname + '/public'));
+app.use('/components', express.static(__dirname + '/components'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/icons', express.static(__dirname + '/icons'));
+app.use(app.router);
+app.set('port', process.env.PORT || 9000);
 
 app.get('/', routes.index);
 
-server.listen(app.get('port'), app.get('ipaddr'), function(){
+server.listen(process.env.PORT || 9000, process.env.IP || undefined, function() {
   console.log('Express server listening on IP/hostname: "' + app.get('ipaddr') + '" and port: "' + app.get('port') + '"');
 });
